03407b22225f4222806807e105c8890fb63c3b9a
lostfictions <lostfictions@users.noreply.github.com>
2021-07-26 15:35:18 -0400

    bodged a fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,45 @@
 /* eslint no-undef: error */
 const withPlugins = require("next-compose-plugins");
 
+const astroturfAltLoaderMatcher = /astroturf\/inline-loader/;
+
+function traverse(rules) {
+  for (const rule of rules) {
+    if (typeof rule.loader === "string" && rule.loader.includes("css-loader")) {
+      if (
+        rule.options &&
+        rule.options.modules &&
+        typeof rule.options.modules.getLocalIdent === "function"
+      ) {
+        const nextGetLocalIdent = rule.options.modules.getLocalIdent;
+        rule.options.modules.getLocalIdent = (
+          context,
+          localIdentName,
+          localName,
+          options
+        ) => {
+          const nextLocalIdent = nextGetLocalIdent(
+            context,
+            localIdentName,
+            localName,
+            options
+          );
+
+          return astroturfAltLoaderMatcher.test(context.request)
+            ? `${nextLocalIdent}_${context.resourceQuery.slice(1)}`
+            : nextLocalIdent;
+        };
+      }
+    }
+    if (typeof rule.use === "object") {
+      traverse(Array.isArray(rule.use) ? rule.use : [rule.use]);
+    }
+    if (Array.isArray(rule.oneOf)) {
+      traverse(rule.oneOf);
+    }
+  }
+}
+
 module.exports = withPlugins(
   [
     () => ({
@@ -11,10 +50,12 @@ module.exports = withPlugins(
           use: [
             {
               loader: "astroturf/loader",
-              // options: { useAltLoader: true },
+              options: { useAltLoader: true },
             },
           ],
         });
+
+        traverse(cfg.module.rules);
         return cfg;
       },
     }),
